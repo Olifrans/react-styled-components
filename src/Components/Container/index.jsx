03b2304ad1c904a0c502810ce9bf0c7be38b841d
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-02-22 16:13:51 -0300

    Items = styled-components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import styled from "styled-components";
+import Extrato from "../Extrato";
 
-const Container = () => {
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
+
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>O ITI - Instituto Tecnológico!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+        <Extrato />
+      </Conteudo>
+    </Container>
   );
 };
-
-export default Container;
